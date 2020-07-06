ee450677d931a05a95bfe54541a0f0d87a5fff7b
atsuhan <atsuhan220@gmail.com>
2020-07-06 15:37:55 +0900

    feat : add projection sample

---
+++ Assets/_Project/Scripts/TextureProjector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+[DefaultExecutionOrder(10000)]
+public class TextureProjector : MonoBehaviour
+{
+    [SerializeField, Range(0.0001f, 179)]
+    private float _fieldOfView = 60;
+    [SerializeField, Range(0.2f, 5.0f)]
+    private float _aspect = 1.0f;
+    [SerializeField, Range(0.0001f, 1000.0f)]
+    private float _nearClipPlane = 0.01f;
+    [SerializeField, Range(0.0001f, 1000.0f)]
+    private float _farClipPlane = 100.0f;
+    [SerializeField]
+    private bool _orthographic = false;
+    [SerializeField]
+    private float _orthographicSize = 1.0f;
+    [SerializeField]
+    private Texture2D _texture;
+
+
+
+    private void Update()
+    {
+        if (_texture == null)
+        {
+            return;
+        }
+        var viewMatrix = Matrix4x4.Scale(new Vector3(1, 1, -1)) * transform.worldToLocalMatrix;
+        Matrix4x4 projectionMatrix;
+        if (_orthographic)
+        {
+            var orthographicWidth = _orthographicSize * _aspect;
+            projectionMatrix = Matrix4x4.Ortho(-orthographicWidth, orthographicWidth, -_orthographicSize, _orthographicSize, _nearClipPlane, _farClipPlane);
+        }
+        else
+        {
+            projectionMatrix = Matrix4x4.Perspective(_fieldOfView, _aspect, _nearClipPlane, _farClipPlane);
+        }
+        projectionMatrix = GL.GetGPUProjectionMatrix(projectionMatrix, true);
+        Shader.SetGlobalMatrix("_ProjectorMatrixVP", projectionMatrix * viewMatrix);
+        Shader.SetGlobalTexture("_ProjectorTexture", _texture);
+        // プロジェクターの位置を渡す
+        // _ObjectSpaceLightPosのような感じでwに0が入っていたらOrthographicの前方方向とみなす
+        var projectorPos = Vector4.zero;
+        projectorPos = _orthographic ? transform.forward : transform.position;
+        projectorPos.w = _orthographic ? 0 : 1;
+        Shader.SetGlobalVector("_ProjectorPos", projectorPos);
+    }
+
+    private void OnDrawGizmos()
+    {
+        var gizmosMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+
+        if (_orthographic)
+        {
+            var orthographicWidth = _orthographicSize * _aspect;
+            var length = _farClipPlane - _nearClipPlane;
+            var start = _nearClipPlane + length / 2;
+            Gizmos.DrawWireCube(Vector3.forward * start, new Vector3(orthographicWidth * 2, _orthographicSize * 2, length));
+        }
+        else
+        {
+            Gizmos.DrawFrustum(Vector3.zero, _fieldOfView, _farClipPlane, _nearClipPlane, _aspect);
+        }
+
+        Gizmos.matrix = gizmosMatrix;
+    }
+}
---
+++ Assets/_Project/Shaders/ProjectionTarget.shader
@@ -0,0 +1,60 @@
+﻿Shader "Custom/ProjectionTarget"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 normal : NORMAL;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float4 projectorSpacePos : TEXCOORD0;
+                float3 worldPos : TEXCOORD1;
+                float3 worldNormal : TEXCOORD2;
+            };
+            
+            sampler2D _ProjectorTexture;
+            float4x4 _ProjectorMatrixVP;
+            float4 _ProjectorPos;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.projectorSpacePos = mul(mul(_ProjectorMatrixVP, unity_ObjectToWorld), v.vertex);
+                o.projectorSpacePos = ComputeScreenPos(o.projectorSpacePos);
+                o.worldNormal = UnityObjectToWorldNormal(v.normal);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                half4 color = 1;
+                i.projectorSpacePos.xyz /= i.projectorSpacePos.w;
+                float2 uv = i.projectorSpacePos.xy;
+                float4 projectorTex = tex2D(_ProjectorTexture, uv);
+                // カメラの範囲外には適用しない
+                fixed3 isOut = step((i.projectorSpacePos - 0.5) * sign(i.projectorSpacePos), 0.5);
+                float alpha = isOut.x * isOut.y * isOut.z;
+                // プロジェクターから見て裏側の面には適用しない
+                alpha *= step(-dot(lerp(-_ProjectorPos.xyz, _ProjectorPos.xyz - i.worldPos, _ProjectorPos.w), i.worldNormal), 0);
+                return projectorTex * alpha;
+            }
+            ENDCG
+        }
+    }
+}
