23ff37de9e44f6134ecb2fd6799ccaed2a99d06d
atsuhan <atsuhan220@gmail.com>
2020-07-13 21:11:23 +0900

    feat : add color map visualization

---
+++ Assets/_Project/Scripts/PowerMapVisualization/PowerMapVisualizationController.cs
@@ -0,0 +1,64 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PowerMapVisualizationController : MonoBehaviour
+{
+    [SerializeField] private int _resolutionX = 100;
+    [SerializeField] private int _resolutionY = 100;
+    [SerializeField] private int _intervalMsec = 10;
+    [SerializeField] private GameObject _targetObj = null;
+
+    private Material _targetMaterial = null;
+    private float[] _powerArr;
+    private ComputeBuffer _powerBuffer = null;
+
+    private int _bufferLength => _resolutionX * _resolutionY;
+
+    private void Start()
+    {
+        InitPowerArrAndBuffer();
+        InitMaterial();
+
+        UpdatePowerData().Forget();
+    }
+
+    private void InitPowerArrAndBuffer()
+    {
+        _powerArr = new float[_bufferLength];
+        _powerBuffer = new ComputeBuffer(_bufferLength, Marshal.SizeOf(typeof(float)));
+    }
+
+    private void InitMaterial()
+    {
+        _targetMaterial = _targetObj.GetComponent<Renderer>().material;
+        _targetMaterial.SetInt("_ResolutionX", _resolutionX);
+        _targetMaterial.SetInt("_ResolutionY", _resolutionY);
+    }
+
+    private async UniTaskVoid UpdatePowerData()
+    {
+        while (true)
+        {
+            for (int x = 0; x < _resolutionX; x++)
+            {
+                for (int y = 0; y < _resolutionY; y++)
+                {
+                    int index = x + y * _resolutionX;
+                    //_powerArr[index] = Mathf.Abs(Mathf.Sin(x / _resolutionX * Mathf.PI + Time.time));
+                    _powerArr[index] = UnityEngine.Random.Range(0f, 1f);
+                }
+            }
+
+            _powerBuffer.SetData(_powerArr);
+            _targetMaterial.SetBuffer("_PowerBuffer", _powerBuffer);
+
+            await UniTask.Delay(_intervalMsec);
+        }
+    }
+
+    private void OnDisable()
+    {
+        _powerBuffer.Release();
+    }
+}
---
+++ Assets/_Project/Shaders/PowerMap/PowerMapVisualization.shader
@@ -0,0 +1,67 @@
+﻿Shader "Custom/PowerMapVisualization"
+{
+    Properties
+    {
+        _ColorMap ("ColorMap", 2D) = "white" {}
+        _Opacity ("Opacity", Float) = 0.5
+    }
+    SubShader
+    {
+        Tags { "Queue" = "Transparent" "RenderType"="Transparent" }
+        LOD 100
+
+        Pass
+        {
+            ZWrite Off
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "/Functions/GetColorFromTexture.h"
+
+            // vert
+            struct appdata
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            // frag
+            sampler2D _ColorMap;
+            float _Opacity;
+
+            int _ResolutionX;
+            int _ResolutionY;
+            StructuredBuffer<float> _PowerBuffer;
+
+            float4 frag (v2f i) : SV_Target
+            {
+                int indexX = floor(i.uv.x * _ResolutionX);
+                int indexY = floor(i.uv.y * _ResolutionY);
+                int bufferId = indexX + indexY *_ResolutionY;
+
+                float4 powerColor = GetColorFromTex(_ColorMap, float2(_PowerBuffer[bufferId], 0));
+                powerColor.a = _Opacity;
+                return powerColor;
+            }
+            ENDCG
+        }
+    }
+}
